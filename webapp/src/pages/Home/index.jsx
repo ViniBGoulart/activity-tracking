569faff1da9b3ba79816ed6dfafdea057f9607f1
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-18 18:10:56 -0300

    fix: temporary modification

--- webapp/src/pages/Home/index.jsx
+++ webapp/src/pages/Home/index.jsx
@@ -1,18 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 import api from "../../services/api";
 
 import "../../styles/globals.css";
 
-import NavBar from "../../components/navbar"
+import NavBar from "../../components/navbar";
+import CardProject from "./CardProject"
 
 export default function Home() {
     const navigate = useNavigate();
 
+    const [projects, setProject] = useState([]);
+    const [token] = useState(localStorage.getItem("token"));
+
+    useEffect(() => {
+        if (![token]) {
+            localStorage.clear();
+            navigate("/login");
+        }
+
+        async function fetchData() {
+            api.get("/api/auth/projects", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then((res) => {
+                    setProject(res.data);
+                })
+                .catch((err) => {
+                    if (
+                        err.request.status &&
+                        err.request.status === (401 || 498)
+                    ) {
+                        localStorage.clear();
+                        navigate("/login");
+                    } else {
+                        alert("Error");
+                    }
+                });
+        }
+
+        fetchData();
+    }, [token]);
+
+    const onInsertProject = (data) => {
+        api.post("/api/auth/projects", data, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => {
+                setProject([...projects], res.data);
+            })
+            .catch((err) => {
+                if (err.status && err.status === (401 || 498)) {
+                    localStorage.clear();
+                    navigate("/login");
+                } else {
+                    alert("Error");
+                }
+            });
+    };
+
     return (
-        <header>
+        <React.Fragment>
             <NavBar />
-        </header>
-    )
+            <CardProject 
+                onInsertProject={onInsertProject}
+            />
+        </React.Fragment>
+    );
 }
