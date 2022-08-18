9e6e9124c81fd1952641fa359f9229fd49a216cd
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-17 22:32:32 -0300

    refactor(routes): based in react-router-dom update
    sent all routes to App.js
    refactor components names

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import Routes from './routes';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 import './styles/globals.css';
 
 export default function App() {
   return (
-    <Routes />
+    <BrowserRouter>
+      <Routes >
+          <Route path="/" element={ <Navigate to="/home" replace /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/register" element={ <Register /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
