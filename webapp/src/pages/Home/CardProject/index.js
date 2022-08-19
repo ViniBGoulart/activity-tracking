5f2f30ba7bc17d05184bc00077201fc4498c2f89
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-19 17:53:41 -0300

    feat(project): button to delete project

--- webapp/src/pages/Home/CardProject/index.js
+++ webapp/src/pages/Home/CardProject/index.js
@@ -1,20 +1,31 @@
 import React from "react";
 
+import api from '../../../services/api'
+
+import Button from '../../../components/Button';
 import CardContainer from "../../../components/CardContainer";
 
 export default function CardProject(props) {
+    const handleSubmit = async (props) => {
+        await props.onDeleteProject({
+            "id": props.id
+        })
+    }
+
     return (
         <CardContainer>
             <div className="flex-col justify-around">
-                <div>
-                    <strong>{props.name}</strong>
+                <div id="cardHeader" className="flex justify-between items-center">
+                    <div>
+                        <strong>{props.name}</strong>
+                    </div>
+                    <Button
+                        onClick={() => handleSubmit(props)}
+                    >X</Button>
                 </div>
-                <div>
+                <div id="cardBody" className="my-2">
                     <small>{props.description}</small>
                 </div>
-            </div>
-            <div>
-
             </div>
         </CardContainer>
     )
--- webapp/src/pages/Home/index.js
+++ webapp/src/pages/Home/index.js
@@ -44,7 +44,7 @@ export default function Home() {
         fetchData();
     }, [token]);
 
-    const onInsertProject = (data) => {
+    const onInsertProject = data => {
         api.post("/api/auth/projects", data, {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -63,6 +63,26 @@ export default function Home() {
             });
     };
 
+    const onDeleteProject = data => {
+        api.delete(`/api/auth/projects/${data.id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then(res => {
+            setProject((prevState) => prevState.filter(obj => {
+                return obj.id !== data.id
+            }));
+        })
+        .catch(err => {
+            if (err.status && err.status === (401 || 498)) {
+                localStorage.clear();
+                navigate("/login");
+            } else {
+                console.log(err)
+            }
+        })
+    };
+
     return (
         <React.Fragment>
             <NavBar />
@@ -74,6 +94,7 @@ export default function Home() {
                     key={project.id}
                     name={project.name}
                     description={project.description}
+                    onDeleteProject={onDeleteProject}
                 />
             ))}
         </React.Fragment>
