759e91152afeb2ee99b9f50b18806b763604f077
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-19 13:00:47 -0300

    fix(mobile navbar): hidden mobile navbar

--- webapp/src/components/navbar/index.js
+++ webapp/src/components/navbar/index.js
@@ -53,7 +53,7 @@ export default function NavBar() {
                         </svg>
                     </button>
                     <div
-                        className="w-full md:block md:w-auto"
+                        className="hidden w-full md:block md:w-auto"
                         id="navbar-default"
                     >
                         <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
