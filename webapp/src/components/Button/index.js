05d13473ea5cb5be58034b98bb8239c27190b3db
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-19 09:36:32 -0300

    feat:(button): navigate option on click

--- webapp/src/components/Button/index.js
+++ webapp/src/components/Button/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export default function Button(props) {
+    const navigate = useNavigate();
+
+    const handleOnClick = () => {
+        if (props.navigate) {
+            return navigate(props.navigate)
+        }
+    }
+
     return (
         <button 
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline"
             type={props.type ?? 'button'}
+            onClick={handleOnClick}
         >
             {props.name}
         </button>
