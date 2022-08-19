5b7c1990256f28b524c803fd753fe660b8b6bad6
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-19 15:10:16 -0300

    feat(card container): cardContainer

--- webapp/src/pages/Home/CardCreateProject/index.js
+++ webapp/src/pages/Home/CardCreateProject/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Button from '../../../components/Button'
+import CardContainer from "../../../components/CardContainer";
 
 export default function CardCreateProject({ onInsertProject }) {
     const [name, setName] = useState('');
@@ -19,36 +20,35 @@ export default function CardCreateProject({ onInsertProject }) {
     }
 
     return (
-        <div className="flex items-center justify-center items-center">
-            <div className="w-full max-w-lg mx-4">
-                <div className="bg-white shadow-md w-full rounded px-8 pt-6 pb-8 mb-4">
-                    <div className="mb-4">
-                        <input
-                            id="projectNameField"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="Name"
-                            type="text"
-                            onChange={e => setName(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <textarea
-                            id="projectDescriptionField"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            placeholder="Description"
-                            type="text"
-                            rows="4"
-                            onChange={e => setDescription(e.target.value)}
-                        />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <Button
-                            name="Send"
-                            onClick={handleSubmit}
-                        />
-                    </div>
+        <React.Fragment>
+            <CardContainer>
+                <h1 className="text-2xl pb-2 font-semibold">Create Project</h1>
+                <div className="mb-4">
+                    <input
+                        id="projectNameField"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        placeholder="Name"
+                        type="text"
+                        onChange={e => setName(e.target.value)}
+                    />
                 </div>
-            </div>
-        </div>
+                <div className="mb-4">
+                    <textarea
+                        id="projectDescriptionField"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        placeholder="Description"
+                        type="text"
+                        rows="4"
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <Button
+                        name="Send"
+                        onClick={handleSubmit}
+                    />
+                </div>
+            </CardContainer>
+        </React.Fragment>
     )
 }
