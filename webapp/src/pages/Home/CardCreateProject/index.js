5965090efbc495654cb03d1676e9502cd0ea6e7d
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-19 11:28:23 -0300

    refactor:(project): change folder name

--- webapp/src/pages/Home/CardCreateProject/index.js
+++ webapp/src/pages/Home/CardCreateProject/index.js
@@ -17,7 +17,7 @@ export default function CardProject({ onInsertProject }) {
 
     return (
         <div className="flex items-center justify-center items-center">
-            <div className="w-full max-w-xs">
+            <div className="w-full max-w-lg">
                 <div className="bg-white shadow-md w-full rounded px-8 pt-6 pb-8 mb-4">
                     <div className="mb-4">
                         <input
@@ -28,10 +28,11 @@ export default function CardProject({ onInsertProject }) {
                         />
                     </div>
                     <div className="mb-4">
-                        <input
+                        <textarea
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="Description"
                             type="text"
+                            rows="4"
                             onChange={e => setDescription(e.target.value)}
                         />
                     </div>
