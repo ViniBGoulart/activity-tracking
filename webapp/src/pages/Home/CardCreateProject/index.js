3923a348ec154f5655665aa1db3669f4f113dc13
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-19 12:51:09 -0300

    fix:(projectInputs): set the value of inputs to empty

--- webapp/src/pages/Home/CardCreateProject/index.js
+++ webapp/src/pages/Home/CardCreateProject/index.js
@@ -12,7 +12,10 @@ export default function CardProject({ onInsertProject }) {
             "description": description
         })
 
+        document.getElementById('projectNameField').value = ''
         setName('')
+        document.getElementById('projectDescriptionField').value = ''
+        setDescription('')
     }
 
     return (
@@ -21,6 +24,7 @@ export default function CardProject({ onInsertProject }) {
                 <div className="bg-white shadow-md w-full rounded px-8 pt-6 pb-8 mb-4">
                     <div className="mb-4">
                         <input
+                            id="projectNameField"
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="Name"
                             type="text"
@@ -29,6 +33,7 @@ export default function CardProject({ onInsertProject }) {
                     </div>
                     <div className="mb-4">
                         <textarea
+                            id="projectDescriptionField"
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             placeholder="Description"
                             type="text"
