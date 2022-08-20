64ffbf0c930403712bba103e8904e9491aac964e
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-20 02:04:23 -0300

    refactor: prettier code formatter

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Home from './pages/Home'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
-import './styles/globals.css';
+import "./styles/globals.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes >
-          <Route path="/" element={ <Navigate to="/home" replace /> } />
-          <Route path="/home" element={ <Home /> } />
-          <Route path="/login" element={ <Login /> } />
-          <Route path="/register" element={ <Register /> } />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={ <Home /> } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
