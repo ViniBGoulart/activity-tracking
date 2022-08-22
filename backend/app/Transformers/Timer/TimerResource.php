c13e1630f3c931adac32e6670d3cf0edaa364e4f
Vinicius Blazius Goulart <viblaziusgoulart@gmail.com>
2022-08-22 18:19:30 -0300

    feat(resource): create timer resource to responses

---
+++ backend/app/Transformers/Timer/TimerResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Transformers\Timer;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TimerResource extends JsonResource
+{
+    private $config;
+
+    public function __construct($resource, $config = array())
+    {
+        parent::__construct($resource);
+        $this->config = $config;
+    }
+
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource['id'],
+            'project_id' => $this->resource['project_id'],
+            'user_id' => $this->resource['user_id'],
+            'name' => $this->resource['name'],
+            'description' => $this->resource['description'],
+            'started_at' => $this->resource['started_at'],
+            'stopped_at' => $this->resource['stopped_at']
+        ];
+    }
+}
