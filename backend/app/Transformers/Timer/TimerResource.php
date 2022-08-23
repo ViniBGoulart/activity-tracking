9942b872aca6a21e18a7e721186246d96647f7a8
Vinicius <viblaziusgoulart@gmail.com>
2022-08-22 21:43:02 -0300

    feat(timers): add additional information in timer response

--- backend/app/Transformers/Timer/TimerResource.php
+++ backend/app/Transformers/Timer/TimerResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers\Timer;
 
+use App\Services\ResponseService;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TimerResource extends JsonResource
@@ -25,5 +26,15 @@ class TimerResource extends JsonResource
             'started_at' => $this->resource['started_at'],
             'stopped_at' => $this->resource['stopped_at']
         ];
+    }
+
+    public function with($request)
+    {
+        return ResponseService::default($this->config, $this->resource['project_id']);
+    }
+
+    public function withResponse($request, $response)
+    {
+        $response->setStatusCode(200);
     }
 }
