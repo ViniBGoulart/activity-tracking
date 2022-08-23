ca07324b01bb53e987d069a74634ac50ccb80c15
Vinicius <viblaziusgoulart@gmail.com>
2022-08-22 22:07:05 -0300

    merge

--- backend/app/Services/ResponseService.php
+++ backend/app/Services/ResponseService.php
@@ -7,17 +7,86 @@ class ResponseService
     /**
      * Default Responses.
      *
-     * @return array
+     * @return array|void
      */
-    public static function default($code, $request = null)
+    public static function default($config = array(), $id = null, $secondId = null)
     {
-        switch ($code) {
-            case 409:
+        $route = $config['route'];
+        switch ($config['type']) {
+            case 'store':
                 return [
-                    'status' => false,
-                    'message' => "Already exists a post opened for this project. Close it before inserting a new one",
-                    'request' => [$request->all()],
+                    'status' => true,
+                    'msg' => 'Success when inserting data',
+                    'url' => $secondId != null ? route($route, $id, $secondId) : ($id != null ? route($route, $id) : route($route))
                 ];
+                break;
+            case 'show':
+                return [
+                    'status' => true,
+                    'msg' => 'Successful Request',
+                    'url' => $secondId != null ? route($route, $id, $secondId) : ($id != null ? route($route, $id) : route($route))
+                ];
+                break;
+            case 'update':
+                return [
+                    'status' => true,
+                    'msg' => 'Successful update',
+                    'url' => $secondId != null ? route($route, $id, $secondId) : ($id != null ? route($route, $id) : route($route))
+                ];
+                break;
+            case 'destroy':
+                return [
+                    'status' => true,
+                    'msg' => 'Successful delete',
+                    'url' => $secondId != null ? route($route, $id, $secondId) : ($id != null ? route($route, $id) : route($route))
+                ];
+                break;
+            case 'stop':
+                return [
+                    'status' => true,
+                    'msg' => 'Successful stopped',
+                    'url' => $secondId != null ? route($route, $id, $secondId) : ($id != null ? route($route, $id) : route($route))
+                ];
+                break;
+        }
+    }
+
+    /**
+     * Register services.
+     *
+     * @return mixed|void
+     */
+    public static function exception($e)
+    {
+        switch ($e->getCode()) {
+            case -403:
+                return response()->json([
+                    'status' => false,
+                    'statusCode' => 403,
+                    'error' => $e->getMessage()
+                ], 403);
+                break;
+            case -404:
+                return response()->json([
+                    'status' => false,
+                    'statusCode' => 404,
+                    'error' => $e->getMessage()
+                ], 404);
+                break;
+            default:
+                if (app()->bound('sentry')) {
+                    $sentry = app('sentry');
+                    $user = auth()->user();
+                    if ($user) {
+                        $sentry->user_context(['id' => $user->id, 'name' => $user->name]);
+                    }
+                    $sentry->captureException($e);
+                }
+                return response()->json([
+                    'status' => false,
+                    'statusCode' => 500,
+                    'error' => 'Error when performing operation',
+                ], 500);
                 break;
         }
     }
